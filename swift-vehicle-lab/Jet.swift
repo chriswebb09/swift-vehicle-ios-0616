a5c853ce86ed96c49d10e944b732836796a927d2
chriswebb09 <chris.webb5249@gmail.com>
2016-07-18 18:59:56 -0400

    new code added

--- swift-vehicle-lab/Jet.swift
+++ swift-vehicle-lab/Jet.swift
@@ -10,4 +10,16 @@ import Foundation
 
 class Jet: Plane {
     
+    override init(name: String, weight: Double, maxSpeed: Double, speed: Double, heading: Double, maxAltitude: Double, altitude: Double) {
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, speed: speed, heading: heading, maxAltitude: maxAltitude, altitude: altitude)
+    }
+    
+    convenience init(name: String, weight: Double, maxSpeed: Double, maxAltitude: Double) {
+        self.init(name: name, weight: weight, maxSpeed: maxSpeed, speed: 0.0, heading: 0.0, maxAltitude: maxAltitude, altitude: 0.0)
+    }
+    
+    func afterBurner() {
+        
+    }
+    
 }
