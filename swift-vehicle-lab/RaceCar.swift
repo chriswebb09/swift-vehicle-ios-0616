fd4735228fa8b542bba124a27942b9f862eb9495
chriswebb09 <chris.webb5249@gmail.com>
2016-07-18 18:48:17 -0400

    new code

--- swift-vehicle-lab/RaceCar.swift
+++ swift-vehicle-lab/RaceCar.swift
@@ -13,7 +13,25 @@ class RaceCar: Car {
     let sponsors: [String]
     
     
-    init() {
+    init(name: String, weight: Double, maxSpeed: Double, speed: Double, heading: Double, transmission: String, cylinders: Int, milesPerGallon: Int, driver: String, sponsors: [String]) {
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, speed: speed, heading: heading, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    
+    override func accelerate() {
+        let speedIncrement = self.maxSpeed * 0.2
+        if (self.speed + speedIncrement) < self.maxSpeed {
+            self.speed += speedIncrement
+        }
+        
+    }
+    
+    override func decelerate() {
+        let speedIncrement = self.maxSpeed * 0.2
+        if self.speed > speedIncrement {
+            self.speed -= speedIncrement
+        }
         
     }
     
